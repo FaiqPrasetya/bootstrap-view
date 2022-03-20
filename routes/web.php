442018da8bac7a2c7f4ecc4dacddbe46c8c13edf
FaiqPrasetya <2041720239@student.polinema.ac.id>
2022-03-20 18:41:53 +0700

    routing added

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function() {
+    return view('index');
+})->name('index');
+
+Route::get('/about', function() {
+    return view('about');
+})->name('about');
+
+Route::get('/shop', function() {
+    return view('shop');
+})->name('shop');
+
+Route::get('/shop-detail', function() {
+    return view('shop-detail');
+})->name('shop-detail');
+
+Route::get('/cart', function() {
+    return view('cart');
+})->name('cart');
+
+Route::get('/checkout', function() {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/my-account', function() {
+    return view('my-account');
+})->name('my-account');
+
+Route::get('/wishlist', function() {
+    return view('wishlist');
+})->name('wishlist');
+
+Route::get('/gallery', function() {
+    return view('gallery');
+})->name('gallery');
+
+Route::get('/contact-us', function() {
+    return view('contact-us');
+})->name('contact-us');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
